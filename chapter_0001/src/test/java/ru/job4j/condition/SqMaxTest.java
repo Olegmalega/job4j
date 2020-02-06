7cf5fccee39aad256053f57b2a187dea1cb01583
Olegmalega <olegmalega3@yandex.ru>
2020-02-06 23:42:16 +0300

    4.5. Отладка программы в IDEA[#217898] исправление

--- chapter_0001/src/test/java/ru/job4j/condition/SqMaxTest.java
+++ chapter_0001/src/test/java/ru/job4j/condition/SqMaxTest.java
@@ -7,22 +7,22 @@ import static org.junit.Assert.*;
 public class SqMaxTest {
     @Test
     public void when6() {
-        int s = SqMax.max(6, 7, 6, 4);
-        assertThat(s, is(6));
+        int s = SqMax.max(4, 3, 2, 1);
+        assertThat(s, is(4));
     }
     @Test
     public void when4() {
         int s = SqMax.max(3, 4, 7, 6);
-        assertThat(s, is(4));
+        assertThat(s, is(7));
     }
     @Test
     public void when7() {
         int s = SqMax.max(8, 3, 7, 6);
-        assertThat(s, is(7));
+        assertThat(s, is(8));
     }
     @Test
-    public void when5() {
-        int s = SqMax.max(3, 4, 2, 5);
+    public void whenall() {
+        int s = SqMax.max(6, 4, 7, 5);
         assertThat(s, is(5));
     }
 }
