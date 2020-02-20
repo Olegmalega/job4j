96cd4660754cc7b425362f3f88571d9d9941d03f
Olegmalega <olegmalega3@yandex.ru>
2020-02-20 22:53:26 +0300

    5.5. Простое число.[#218017].исправленное

--- chapter_0001/src/main/java/ru/job4j/loop/ChecckPrimeNumber.java
+++ chapter_0001/src/main/java/ru/job4j/loop/ChecckPrimeNumber.java
@@ -1,18 +1,16 @@
 package ru.job4j.loop;
 
-import java.util.spi.AbstractResourceBundleProvider;
-
 public class ChecckPrimeNumber {
     public static boolean check(int number) {
-        for (int x = 2; x < number; x++) {
-            if ((number % x) == 0) {
-                break;}
-            {
-               return true;
+            boolean prime = true;
+            if (number == 1)
+                prime = false;
+            for (int x = 2; x <= number / 2; x++) {
+                if (number % x == 0) {
+                    prime = false;
+                    break;
+                }
             }
+            return prime;
         }
-        return false;
-
     }
-
-}
--- chapter_0001/src/test/java/ru/job4j/loop/ChecckPrimeNumberTest.java
+++ chapter_0001/src/test/java/ru/job4j/loop/ChecckPrimeNumberTest.java
@@ -6,21 +6,76 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class ChecckPrimeNumberTest {
+  //  @Test
+ //   public void when5() {
+ //       boolean rsl = ChecckPrimeNumber.check(5);
+  //      assertThat(rsl, is(true));
+  //  }
+
+ //   @Test
+ //   public void when4() {
+ //       boolean rsl = ChecckPrimeNumber.check(4);
+  //      assertThat(rsl, is(false));
+  //  }
+
+  //  @Test
+ //   public void when1() {
+ //       boolean rsl = ChecckPrimeNumber.check(1);
+  //      assertThat(rsl, is(false));
+ //   }
     @Test
-    public void when5() {
-        boolean rsl = ChecckPrimeNumber.check(5);
+    public void when1() {
+        boolean rsl = ChecckPrimeNumber.check(1);
+        assertThat(rsl, is(false));
+    }
+    @Test
+    public void when2() {
+        boolean rsl = ChecckPrimeNumber.check(2);
         assertThat(rsl, is(true));
     }
-
+    @Test
+    public void when3() {
+        boolean rsl = ChecckPrimeNumber.check(3);
+        assertThat(rsl, is(true));
+    }
     @Test
     public void when4() {
         boolean rsl = ChecckPrimeNumber.check(4);
         assertThat(rsl, is(false));
     }
-
     @Test
-    public void when1() {
-        boolean rsl = ChecckPrimeNumber.check(1);
+    public void when5() {
+        boolean rsl = ChecckPrimeNumber.check(5);
+        assertThat(rsl, is(true));
+    }
+    @Test
+    public void when6() {
+        boolean rsl = ChecckPrimeNumber.check(6);
         assertThat(rsl, is(false));
+    }
+    @Test
+    public void when7() {
+        boolean rsl = ChecckPrimeNumber.check(7);
+        assertThat(rsl, is(true));
+    }
+    @Test
+    public void when8() {
+        boolean rsl = ChecckPrimeNumber.check(8);
+        assertThat(rsl, is(false));
+    }
+    @Test
+    public void when9() {
+        boolean rsl = ChecckPrimeNumber.check(9);
+        assertThat(rsl, is(false));
+    }
+    @Test
+    public void when10() {
+        boolean rsl = ChecckPrimeNumber.check(10);
+        assertThat(rsl, is(false));
+    }
+    @Test
+    public void when11() {
+        boolean rsl = ChecckPrimeNumber.check(11);
+        assertThat(rsl, is(true));
     }
 }
